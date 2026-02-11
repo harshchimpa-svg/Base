e6e91d70a113d0399ceb6702a9c7d4ebb06e4800
Harsh <harshchimpa@gmail.com>
2026-02-11 16:05:25 +0530

    chane 2

---
+++ Peristance/Migrations/20260203161119_UserProfile.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "UserProfile");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "UserProfile",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
